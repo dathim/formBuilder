905f147362e0ba5ae4f27759114cce83abdbd81c
dathim <dathim@gmail.com>
2021-10-20 22:55:19 +0300

    make migrations

---
+++ database/migrations/2021_10_20_194358_form_fields.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FormFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_field', function (Blueprint $table) {
+            $table->id();
+            $table->integer('field_id');
+            $table->string('name');
+            $table->boolean('required');
+            $table->integer('min_length');
+            $table->integer('max_lentgh');
+            $table->longText('suggest');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_fields');
+    }
+}
